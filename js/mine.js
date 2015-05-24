8749a63dd8cac99bff0857761c691c4a5a2c85af
Iuri Matias <iuri.matias@gmail.com>
2015-05-24 09:36:19 -0400

    mine a minimal amount

--- js/mine.js
+++ js/mine.js
@@ -1,6 +1,8 @@
 
 setInterval(function() {
-  if(web3.eth.pendingTransactions().length > 0 && !web3.eth.mining) {
+
+  var minimalAmount = (web3.eth.getBalance(web3.eth.coinbase) >= 1500000000000000000);
+  if(!web3.eth.mining && (!minimalAmount || web3.eth.pendingTransactions().length > 0)) {
     admin.miner.start();
   }
   else {
