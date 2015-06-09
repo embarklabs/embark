31fdc58cfe1682b33dc74710a90553fd92428828
Iuri Matias <iuri.matias@gmail.com>
2015-06-09 08:22:18 -0400

    require needed packages for embark; cleanup boilerplate package

---
+++ index.js
@@ -0,0 +1,10 @@
+var hashmerge = require('hashmerge');
+var readYaml = require('read-yaml');
+var shelljs = require('shelljs');
+var shelljs_global = require('shelljs/global');
+var web3 = require('web3');
+var express = require('express');
+var compression = require('compression');
+var commander = require('commander');
+var wrench = require('wrench');
+console.log("dude!!!");
