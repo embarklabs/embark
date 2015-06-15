619a92f71b6c0756ed99b8c56d2e6d6c07eb6978
Iuri Matias <iuri.matias@gmail.com>
2015-06-15 06:07:40 -0400

    update dependencies

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ var express = require('express');
 var compression = require('compression');
 var commander = require('commander');
 var wrench = require('wrench');
+var python = require('python');
+var syncMe = require('sync-me');
+var methodmissing = require('methodmissing');
+var jasmine = require('jasmine');
+
 console.log("embark required");
 
 embark = {}
