a764b6294204ef5da1445ff0703e902cc48621a6
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-06-30 17:03:47 +0200

    web3.js 0.7.1

--- js/web3.js
+++ js/web3.js
@@ -354,9 +354,10 @@ var formatInputBytes = function (value) {
  * @returns {SolidityParam}
  */
 var formatInputDynamicBytes = function (value) {
-    value = utils.toHex(value);
-    var result = utils.padRight((value).substr(2), 64);
-    var length = Math.floor(value.length / 2 - 1);
+    value = utils.toHex(value).substr(2);
+    var l = Math.floor((value.length + 63) / 64);
+    var result = utils.padRight(value, l * 64);
+    var length = Math.floor(value.length / 2);
     return new SolidityParam(formatInputInt(length).value + result, 32);
 };
 
@@ -368,7 +369,9 @@ var formatInputDynamicBytes = function (value) {
  * @returns {SolidityParam}
  */
 var formatInputString = function (value) {
-    var result = utils.fromAscii(value, c.ETH_PADDING).substr(2);
+    var result = utils.fromAscii(value).substr(2);
+    var l = Math.floor((result.length + 63) / 64);
+    result = utils.padRight(result, l * 64);
     return new SolidityParam(formatInputInt(value.length).value + result, 32);
 };
 
@@ -721,13 +724,14 @@ var getOffset = function (bytes, index) {
  */
 SolidityParam.decodeBytes = function (bytes, index) {
     index = index || 0;
-    //TODO add support for strings longer than 32 bytes
-    //var length = parseInt('0x' + bytes.substr(offset * 64, 64));
 
     var offset = getOffset(bytes, index);
 
-    // 2 * , cause we also parse length
-    return new SolidityParam(bytes.substr(offset * 2, 2 * 64), 0);
+    var l = parseInt('0x' + bytes.substr(offset * 2, 64));
+    l = Math.floor((l + 31) / 32);
+
+    // (1 + l) * , cause we also parse length
+    return new SolidityParam(bytes.substr(offset * 2, (1 + l) * 64), 0);
 };
 
 /**
@@ -1394,7 +1398,7 @@ module.exports = {
 
 },{"bignumber.js":"bignumber.js"}],8:[function(require,module,exports){
 module.exports={
-    "version": "0.7.0"
+    "version": "0.7.1"
 }
 
 },{}],9:[function(require,module,exports){
@@ -2635,6 +2639,7 @@ var Filter = function (options, methods, formatter, callback) {
     });
     this.options = getOptions(options);
     this.implementation = implementation;
+    this.filterId = null;
     this.callbacks = [];
     this.pollFilters = [];
     this.formatter = formatter;
@@ -2645,12 +2650,13 @@ var Filter = function (options, methods, formatter, callback) {
             });
         } else {
             self.filterId = id;
-            // get filter logs at start
+
+            // get filter logs for the already existing watch calls
             self.callbacks.forEach(function(cb){
                 getLogsAtStart(self, cb);
             });
-            pollFilter(self);
-
+            if(self.callbacks.length > 0)
+                pollFilter(self);
 
             // start to watch immediately
             if(callback) {
@@ -3117,8 +3123,9 @@ SolidityFunction.prototype.request = function () {
     var format = this.unpackOutput.bind(this);
     
     return {
+        method: this._constant ? 'eth_call' : 'eth_sendTransaction',
         callback: callback,
-        payload: payload, 
+        params: [payload], 
         format: format
     };
 };
