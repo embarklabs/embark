95823a8e5401bb48ec0eecf3643af5552fae939c
Iuri Matias <iuri.matias@gmail.com>
2015-10-08 15:30:47 -0400

    make deployment async

--- demo/spec/contracts/simple_storage_spec.js
+++ demo/spec/contracts/simple_storage_spec.js
@@ -1,4 +1,6 @@
 var Embark = require('embark-framework');
+var ethersim = require('ethersim');
+var web3 = require('web3');
 Embark.init();
 Embark.blockchainConfig.loadConfigFile('config/blockchain.yml');
 Embark.contractsConfig.loadConfigFile('config/contracts.yml');
@@ -6,7 +8,17 @@ Embark.contractsConfig.loadConfigFile('config/contracts.yml');
 var files = ["app/contracts/simple_storage.sol"];
 
 Embark.contractsConfig.init(files, 'development');
-var EmbarkSpec = Embark.tests(files);
+
+var Manager = ethersim.Manager;
+var Provider = ethersim.Provider;
+
+console.log("initializing");
+var manager = new Manager();
+
+web3.setProvider(new Provider(manager));
+abi = Embark.deployContracts('development', files, "/tmp/abi.js", "chains.json", web3);
+console.log(abi);
+eval(abi);
 
 describe("SimpleStorage", function() {
   beforeAll(function() {
