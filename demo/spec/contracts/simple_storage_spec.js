b4d659a3d157471ced71b65cf9d4e0656ab9703c
Iuri Matias <iuri.matias@gmail.com>
2015-10-09 10:57:30 -0400

    move spec code to lib

--- demo/spec/contracts/simple_storage_spec.js
+++ demo/spec/contracts/simple_storage_spec.js
@@ -1,31 +1,8 @@
-var Embark = require('embark-framework');
-var ethersim = require('ethersim');
-var web3 = require('web3');
-
-var Manager = ethersim.Manager;
-var Provider = ethersim.Provider;
-
-var manager = new Manager();
-web3.setProvider(new Provider(manager));
-
-Embark.init(web3);
-Embark.blockchainConfig.loadConfigFile('config/blockchain.yml');
-Embark.contractsConfig.loadConfigFile('config/contracts.yml');
-
-var files = ["app/contracts/simple_storage.sol"];
-
-Embark.contractsConfig.init(files, 'development');
-
-console.log("initializing");
+var EmbarkSpec = require('embark-framework').test;
 
 describe("SimpleStorage", function() {
   beforeAll(function(done) {
-    Embark.deployContracts('development', files, "/tmp/abi.js", "chains.json", false, function(abi) {
-      console.log("return abi");
-      console.log(abi);
-      eval(abi);
-      done();
-    });
+    EmbarkSpec(done);
     //SimpleStorage = EmbarkSpec.request("SimpleStorage", [150]);
   });
 
