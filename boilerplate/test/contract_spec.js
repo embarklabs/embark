58c0244a4395049aed8655543f7a89068a52974c
Iuri Matias <iuri.matias@gmail.com>
2016-10-02 19:13:46 -0400

    clean up

--- boilerplate/test/contract_spec.js
+++ boilerplate/test/contract_spec.js
@@ -3,32 +3,30 @@ var Embark = require('embark-framework');
 var EmbarkSpec = Embark.initTests();
 var web3 = EmbarkSpec.web3;
 
-describe("MyContract", function() {
-  // Deploy all contrats using the development config
-  before(function(done) {
-    EmbarkSpec.sim.createAccounts(10, function() {
-      EmbarkSpec.sim.setBalance(web3.eth.accounts[0], 1000000000000000000000, function() {
-        EmbarkSpec.deployAll(done);
-      });
-    });
-  });
-
-  // example usage
-  //it("should set constructor value", function(done) {
-  //  SimpleStorage.storedData(function(err, result) {
-  //    assert.equal(result.toNumber(), 100);
-  //    done();
-  //  });
-  //});
-
-  //it("set storage value", function(done) {
-  //  SimpleStorage.set(150, function() {
-  //    SimpleStorage.get(function(err, result) {
-  //      assert.equal(result.toNumber(), 150);
-  //      done();
-  //    });
-  //  });
-  //});
-
-})
-
+//describe("SimpleStorage", function() {
+//  before(function(done) {
+//    var contractsConfig = {
+//      "SimpleStorage": {
+//        args: [100, '0x123']
+//      }
+//    };
+//    EmbarkSpec.deployAll(contractsConfig, done);
+//  });
+//
+//  it("should set constructor value", function(done) {
+//    SimpleStorage.storedData(function(err, result) {
+//      assert.equal(result.toNumber(), 100);
+//      done();
+//    });
+//  });
+//
+//  it("set storage value", function(done) {
+//    SimpleStorage.set(150, function() {
+//      SimpleStorage.get(function(err, result) {
+//        assert.equal(result.toNumber(), 150);
+//        done();
+//      });
+//    });
+//  });
+//
+//});
