73142e9cd4123e6bc1f26ee44e77fa161fd35ac3
Iuri Matias <iuri.matias@gmail.com>
2017-01-10 06:53:58 -0500

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,12 @@
 # sys.path.insert(0, os.path.abspath('.'))
 
 
+import sphinx_rtd_theme
+
+html_theme = "sphinx_rtd_theme"
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
