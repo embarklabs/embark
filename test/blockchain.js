7599f43a3b697fd9a37d0fc590e7869e6a041691
Todd Baur <todd@toadkicker.com>
2017-03-30 20:48:22 +0900

    fixed problem with double this.client in blockchain.js

--- test/blockchain.js
+++ test/blockchain.js
@@ -1,47 +1,45 @@
 /*globals describe, it*/
 const Blockchain = require('../lib/cmds/blockchain/blockchain');
-// let BlockchainClient = require('../lib/cmds/blockchain/blockchain_client');
+
 const assert = require('assert');
 
-describe('embark.Blockchain', function() {
+describe('embark.Blockchain', function () {
   //let Client = function() {};
   //Client.prototype.name = "ClientName";
 
-  describe('#initializer', function() {
+  describe('#initializer', function () {
     //let client = new Client();
 
-    describe('with empty config', function() {
-      it('should have a default config', function() {
+    describe('with empty config', function () {
+      it('should have a default config', function () {
         let config = {
-          blockchainConfig: {
-            networkType: 'custom',
-            genesisBlock: false,
-            geth_bin: 'geth',
-            datadir: false,
-            mineWhenNeeded: false,
-            rpcHost: 'localhost',
-            rpcPort: 8545,
-            rpcApi: ['eth', 'web3', 'net'],
-            rpcCorsDomain: false,
-            networkId: 12301,
-            port: 30303,
-            nodiscover: false,
-            maxpeers: 25,
-            mine: false,
-            vmdebug: false,
-            whisper: true,
-            account: {},
-            bootnodes: "",
-          }
-        }
+          networkType: 'custom',
+          genesisBlock: false,
+          geth_bin: 'geth',
+          datadir: false,
+          mineWhenNeeded: false,
+          rpcHost: 'localhost',
+          rpcPort: 8545,
+          rpcApi: ['eth', 'web3', 'net'],
+          rpcCorsDomain: false,
+          networkId: 12301,
+          port: 30303,
+          nodiscover: false,
+          maxpeers: 25,
+          mine: false,
+          vmdebug: false,
+          whisper: true,
+          account: {},
+          bootnodes: ""
+        };
         let blockchain = new Blockchain(config, 'geth');
 
         assert.deepEqual(blockchain.config, config);
       });
     });
 
-    describe('with config', function() {
-      it('should take config params', function() {
+    describe('with config', function () {
+      it('should take config params', function () {
         let config = {
           networkType: 'livenet',
           genesisBlock: 'foo/bar/genesis.json',
