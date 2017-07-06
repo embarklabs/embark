232f6fc88b7223ae64b97099e07a5f122b8d58b9
Iuri Matias <iuri.matias@gmail.com>
2017-07-06 18:48:20 -0400

    support list of fallbacks for dapp web3 connection, support separate connection config for deployment

--- test_app/app/contracts/another_storage.sol
+++ test_app/app/contracts/another_storage.sol
@@ -1,6 +1,7 @@
-pragma solidity ^0.4.7;
+pragma solidity ^0.4.11;
 contract AnotherStorage {
   address public simpleStorageAddress;
+  address simpleStorageAddress2;
 
   function AnotherStorage(address addr) {
     simpleStorageAddress = addr;
