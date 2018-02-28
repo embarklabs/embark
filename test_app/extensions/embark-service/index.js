62d4dd456df25b4f5cbc8ded1c604f89c5c774bf
Iuri Matias <iuri.matias@gmail.com>
2018-02-28 17:45:39 -0500

    restore pipeline plugin

--- test_app/extensions/embark-service/index.js
+++ test_app/extensions/embark-service/index.js
@@ -1,5 +1,12 @@
+var Haml = require('haml');
+
 module.exports = function(embark) {
   embark.registerServiceCheck('PluginService', function(cb) {
     cb({name: "ServiceName", status: "on"});
+  });
+
+  embark.registerPipeline((embark.pluginConfig.files || ['**/*.haml']), function(opts) {
+    var source = opts.source;
+    return Haml.render(source);
   });
 };
