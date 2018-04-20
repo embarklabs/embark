9bf06aebce961603c64794d6d929a4a74a28ca2e
Jonathan Rainville <rainville.jonathan@gmail.com>
2018-04-20 10:03:03 -0400

    refacotr how we handle files already parsed

--- test/file.js
+++ test/file.js
@@ -59,5 +59,27 @@ describe('embark.File', function () {
         done();
       });
     });
+
+    it('should find all the imports but only once if called twice', function (done) {
+      const contract = fs.readFileSync('./test/contracts/contract_with_http_import.sol').toString();
+      const file = new File({filename: '.embark/contracts/embark-framework/embark/master/test_app/app/contracts/simple_storage.sol',
+        path: 'https://raw.githubusercontent.com/embark-framework/embark/develop/test_apps/test_app/app/contracts/simple_storage.sol'});
+      const downloadFileStub = sinon.stub(file, 'downloadFile')
+        .callsFake((path, url, cb) => {
+          cb();
+        });
+
+      file.parseFileForImport(contract, () => {
+        // Parse again
+        file.parseFileForImport(contract, () => {
+          assert.strictEqual(downloadFileStub.callCount, 1);
+          assert.strictEqual(downloadFileStub.firstCall.args[0],
+            '.embark/contracts/embark-framework/embark/develop/test_apps/contracts_app/contracts/contract_args.sol');
+          assert.strictEqual(downloadFileStub.firstCall.args[1],
+            'https://raw.githubusercontent.com/embark-framework/embark/develop/test_apps/contracts_app/contracts/contract_args.sol');
+          done();
+        });
+      });
+    });
   });
 });
