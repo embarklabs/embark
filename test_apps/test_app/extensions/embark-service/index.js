e4566f9f817ab15156febf4130e34abff1b483fb
Jonathan Rainville <rainville.jonathan@gmail.com>
2018-04-25 16:27:47 -0400

    add more registers but generation one fails in run

--- test_apps/test_app/extensions/embark-service/index.js
+++ test_apps/test_app/extensions/embark-service/index.js
@@ -23,8 +23,33 @@ module.exports = function (embark) {
   embark.addFileToPipeline('./fileInPipeline.js');
   embark.addFileToPipeline('./fileInPipeline.js', 'js/fileInPipeline.js');
 
-  embark.registerBeforeDeploy(function(options, callback) {
+  embark.registerBeforeDeploy(function (options, callback) {
     // Just calling register to prove it works. We don't actually want to change the contracts
     callback({contractCode: options.contract.code});
   });
+
+  embark.registerClientWeb3Provider(function(options) {
+    return "web3 = new Web3(new Web3.providers.HttpProvider('http://" + options.rpcHost + ":" + options.rpcPort + "'));";
+  });
+
+  /*embark.registerContractsGeneration(function (options) {
+    const contractGenerations = [];
+    Object.keys(options.contracts).map(className => {
+      const contract = options.contracts[className];
+      const abi = JSON.stringify(contract.abiDefinition);
+
+      contractGenerations.push(`${className} = new this.web3.eth.contract('${abi}').at('${contract.deployedAddress}')`);
+    });
+    return contractGenerations.join('\n');
+    // return '';
+  });*/
+
+  embark.registerConsoleCommand((cmd) => {
+    if (cmd === "hello") {
+      return "hello there!";
+    }
+    // continue to embark or next plugin;
+    return false;
+  });
+
 };
