9e1778f62c4fa3de9874fbd8288039b95ad7e319
Iuri Matias <iuri.matias@gmail.com>
2018-05-10 19:57:26 -0400

    comment web3 test override to avoid confusion in the test_app

--- test_apps/test_app/extensions/embark-service/index.js
+++ test_apps/test_app/extensions/embark-service/index.js
@@ -28,9 +28,10 @@ module.exports = function (embark) {
     callback({contractCode: options.contract.code});
   });
 
-  embark.registerClientWeb3Provider(function(options) {
-    return "web3 = new Web3(new Web3.providers.HttpProvider('http://" + options.rpcHost + ":" + options.rpcPort + "'));";
-  });
+  // NOTE: uncommenting this will make dappConnection stop working
+  //embark.registerClientWeb3Provider(function(options) {
+  //  return "web3 = new Web3(new Web3.providers.HttpProvider('http://" + options.rpcHost + ":" + options.rpcPort + "'));";
+  //});
 
   embark.registerConsoleCommand((cmd) => {
     if (cmd === "hello") {
