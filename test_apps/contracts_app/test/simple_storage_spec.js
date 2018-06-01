8664570d7ca7525e7379061df7cd37a5ead0f419
Jonathan Rainville <rainville.jonathan@gmail.com>
2018-06-01 13:43:43 -0400

    conflict in test

--- test_apps/contracts_app/test/simple_storage_spec.js
+++ test_apps/contracts_app/test/simple_storage_spec.js
@@ -1,26 +1,33 @@
-/*global contract, config, it, embark*/
+/*global contract, before, it, embark, web3*/
 const assert = require('assert');
-const SimpleStorage = embark.require('Embark/contracts/SimpleStorage');
-
-config({
-  contracts: {
-    "SimpleStorage": {
-      args: [100]
-    }
-  }
-});
+const SimpleStorage = embark.require('contracts/SimpleStorage');
 
 contract("SimpleStorage", function () {
   this.timeout(0);
 
+  before(function (done) {
+    const contractsConfig = {
+      contracts: {
+        "SimpleStorage": {
+          args: [100]
+        }
+      }
+    };
+    embark.config(contractsConfig, () => {
+      done();
+    });
+  });
+
   it("should set constructor value", async function () {
-    let result = await SimpleStorage.methods.storedData().call();
+    let result = await SimpleStorage.contract.methods.storedData().call();
     assert.strictEqual(parseInt(result, 10), 100);
   });
 
   it("set storage value", async function () {
-    await SimpleStorage.methods.set(150).send();
-    let result = await SimpleStorage.methods.get().call();
+    // TODO Solve from
+    await SimpleStorage.contract.methods.set(150).send({from: web3.eth.defaultAccount});
+    let result = await SimpleStorage.contract.methods.get().call();
     assert.strictEqual(parseInt(result, 10), 499650);
   });
+
 });
