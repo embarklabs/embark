4ffb5c401fae64e24927fa2838a576fc20e9caf4
Jonathan Rainville <rainville.jonathan@gmail.com>
2018-06-06 16:43:08 -0400

    enable setting balance in mutliple formats

--- test_apps/test_app/test/another_storage_spec.js
+++ test_apps/test_app/test/another_storage_spec.js
@@ -1,13 +1,16 @@
-/*global contract, config, it, embark*/
+/*global contract, config, it, embark, web3*/
 const assert = require('assert');
 const AnotherStorage = embark.require('Embark/contracts/AnotherStorage');
 const SimpleStorage = embark.require('Embark/contracts/SimpleStorage');
+
+let accounts;
 
 config({
   deployment: {
     "accounts": [
       {
-        "mnemonic": "example exile argue silk regular smile grass bomb merge arm assist farm"
+        "mnemonic": "example exile argue silk regular smile grass bomb merge arm assist farm",
+        balance: "5ether"
       }
     ]
   },
@@ -19,6 +22,8 @@ config({
       args: ["$SimpleStorage"]
     }
   }
+}, (err, theAccounts) => {
+  accounts = theAccounts;
 });
 
 contract("AnotherStorage", function() {
@@ -27,5 +32,11 @@ contract("AnotherStorage", function() {
   it("set SimpleStorage address", async function() {
     let result = await AnotherStorage.methods.simpleStorageAddress().call();
     assert.equal(result.toString(), SimpleStorage.options.address);
+  });
+
+  it('should set the balance correctly', async function () {
+    const balance = await web3.eth.getBalance(accounts[0]);
+    assert.ok(balance < 5000000000000000000);
+    assert.ok(balance > 4000000000000000000);
   });
 });
