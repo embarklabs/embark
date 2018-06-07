8fa7357d07aff8b9b2cb7116a9243d443e89260b
Iuri Matias <iuri.matias@gmail.com>
2018-06-07 19:08:18 -0400

    fix default from and data; add single test spec

---
+++ test_apps/test_app/test/simple_storage_deploy_spec.js
@@ -0,0 +1,33 @@
+/*global contract, config, it, embark, assert, web3*/
+const SimpleStorage = embark.require('Embark/contracts/SimpleStorage');
+let accounts;
+
+config({
+  contracts: {
+    "SimpleStorage": {
+      args: [100]
+    }
+  }
+}, (err, theAccounts) => {
+  accounts = theAccounts;
+});
+
+contract("SimpleStorage Deploy", function () {
+  let SimpleStorageInstance;
+
+  before(async function() {
+    SimpleStorageInstance = await SimpleStorage.deploy({data: SimpleStorage.options.data, arguments: [150]}).send();
+  });
+
+  it("should set constructor value", async function () {
+    let result = await SimpleStorageInstance.methods.storedData().call();
+    assert.strictEqual(parseInt(result, 10), 150);
+  });
+
+  it("set storage value", async function () {
+    await SimpleStorageInstance.methods.set(150).send();
+    let result = await SimpleStorageInstance.methods.get().call();
+    assert.strictEqual(parseInt(result, 10), 499650);
+  });
+
+});
