17640e51de3d8992dfe4cecf7fab8ffc6940c0b4
Iuri Matias <iuri.matias@gmail.com>
2018-07-07 18:29:45 +0300

    move setProviders to EmbarkJS

--- js/embark.js
+++ js/embark.js
@@ -1,3 +1,5 @@
+import {detectSeries} from 'async';
+
 var EmbarkJS = {
   onReady: function (cb) {
     if (typeof (__embarkContext) === 'undefined') {
@@ -298,6 +300,40 @@ EmbarkJS.Storage.isAvailable = function () {
     throw new Error('Storage provider not set; e.g EmbarkJS.Storage.setProvider("ipfs")');
   }
   return this.currentStorage.isAvailable();
+};
+
+let __embarkStorage = {};
+
+EmbarkJS.Storage.setProviders = async function (dappConnOptions) {
+  try {
+    await detectSeries(dappConnOptions, async (dappConn, callback) => {
+      if(dappConn === '$BZZ' || dappConn.provider === 'swarm'){
+        let options = dappConn;
+        if(dappConn === '$BZZ') options = {"useOnlyGivenProvider": true};
+        try{
+          await EmbarkJS.Storage.setProvider('swarm', options);
+          let isAvailable = await EmbarkJS.Storage.isAvailable();
+          callback(null, isAvailable);
+        }catch(err){
+          callback(null, false); // catch errors for when bzz object not initialised but config has requested it to be used
+        }
+      }
+      else if(dappConn.provider === 'ipfs') {
+        // set the provider then check the connection, if true, use that provider, else, check next provider
+        try{
+          await EmbarkJS.Storage.setProvider('ipfs', dappConn);
+          let isAvailable =  await EmbarkJS.Storage.isAvailable();
+          callback(null, isAvailable);
+        } catch(err) {
+          callback(null, false); // catch but keep looping by not passing err to callback
+        }
+      }
+    }, function(err, result){
+      if(!result) throw new Error('Could not connect to a storage provider using any of the dappConnections in the storage config');
+    });
+  } catch (err) {
+    throw new Error('Failed to connect to a storage provider: ' + err.message);
+  }
 };
 
 EmbarkJS.Messages = {};
