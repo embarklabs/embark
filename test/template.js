611c2198a6367a3e2b57261f22b07330324200a6
Iuri Matias <iuri.matias@gmail.com>
2018-07-09 20:24:07 +0300

    update test so it plays nice with windows

--- test/template.js
+++ test/template.js
@@ -15,37 +15,37 @@ describe('TemplateGenerator', function () {
       it('return correct zip filename for https link', function () {
         let result = templateGenerator.getExternalProject("https://github.com/embark-framework/embark");
         assert.strictEqual(result.url, "https://github.com/embark-framework/embark/archive/master.zip");
-        assert.strictEqual(result.filePath, ".embark/templates/embark-framework/embark/archive.zip");
+        assert.strictEqual(result.filePath.replace(/\\/g,'/'), ".embark/templates/embark-framework/embark/archive.zip");
       });
 
       it('return correct zip filename for http link', function () {
         let result = templateGenerator.getExternalProject("http://github.com/embark-framework/embark");
         assert.strictEqual(result.url, "http://github.com/embark-framework/embark/archive/master.zip");
-        assert.strictEqual(result.filePath, ".embark/templates/embark-framework/embark/archive.zip");
+        assert.strictEqual(result.filePath.replace(/\\/g,'/'), ".embark/templates/embark-framework/embark/archive.zip");
       });
 
       it('return correct zip filename without protocol specified ', function () {
         let result = templateGenerator.getExternalProject("github.com/embark-framework/embark");
         assert.strictEqual(result.url, "https://github.com/embark-framework/embark/archive/master.zip");
-        assert.strictEqual(result.filePath, ".embark/templates/embark-framework/embark/archive.zip");
+        assert.strictEqual(result.filePath.replace(/\\/g,'/'), ".embark/templates/embark-framework/embark/archive.zip");
       });
 
       it('return correct zip filename without protocol specified ', function () {
         let result = templateGenerator.getExternalProject("github.com/embark-framework/embark");
         assert.strictEqual(result.url, "https://github.com/embark-framework/embark/archive/master.zip");
-        assert.strictEqual(result.filePath, ".embark/templates/embark-framework/embark/archive.zip");
+        assert.strictEqual(result.filePath.replace(/\\/g,'/'), ".embark/templates/embark-framework/embark/archive.zip");
       });
 
       it('return correct zip filename with just username/repo specified', function () {
         let result = templateGenerator.getExternalProject("embark-framework/embark");
         assert.strictEqual(result.url, "https://github.com/embark-framework/embark/archive/master.zip");
-        assert.strictEqual(result.filePath, ".embark/templates/embark-framework/embark/archive.zip");
+        assert.strictEqual(result.filePath.replace(/\\/g,'/'), ".embark/templates/embark-framework/embark/archive.zip");
       });
 
       it('return correct zip filename with just embark template specified', function () {
         let result = templateGenerator.getExternalProject("react");
         assert.strictEqual(result.url, "https://github.com/embark-framework/embark-react-template/archive/master.zip");
-        assert.strictEqual(result.filePath, ".embark/templates/embark-framework/embark-react-template/archive.zip");
+        assert.strictEqual(result.filePath.replace(/\\/g,'/'), ".embark/templates/embark-framework/embark-react-template/archive.zip");
       });
 
     });
