05d1a6fe3aada226bfcf338c8bda0a254e0b2017
Richard Ramos <info@richardramos.me>
2018-07-16 10:23:11 -0400

    Symmetric and private keys can be set in whisper

--- templates/boilerplate/config/communication.js
+++ templates/boilerplate/config/communication.js
@@ -36,5 +36,12 @@ module.exports = {
   // "embark run custom_name"
   //custom_name: {
   //}
+    // Use this section when you need a specific symmetric or private keys in whisper
+    /*
+    ,keys: {
+      symmetricKey: "your_symmetric_key",// Symmetric key for message decryption
+      privateKey: "your_private_key" // Private Key to be used as a signing key and for message decryption
+    }
+    */
+  //}
 };
-
