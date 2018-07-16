8997e826c557f7050377cc547dc060143335cc42
Richard Ramos <info@richardramos.me>
2018-07-16 10:23:11 -0400

    Symmetric and private keys can be set in whisper

--- templates/demo/config/communication.js
+++ templates/demo/config/communication.js
@@ -8,5 +8,12 @@ module.exports = {
       port: 8546, // Port of the blockchain node
       type: "ws" // Type of connection (ws or rpc)
     }
+    // Use this section when you need a specific symmetric or private keys in whisper
+    /*
+    ,keys: {
+      symmetricKey: "your_symmetric_key",// Symmetric key for message decryption
+      privateKey: "your_private_key" // Private Key to be used as a signing key and for message decryption
+    }
+    */
   }
 };
