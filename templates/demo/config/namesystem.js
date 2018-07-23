89421771ef2eae8e78e58ab8cdf769071c44a0e1
Jonathan Rainville <rainville.jonathan@gmail.com>
2018-07-23 17:11:41 -0400

    add register in demo

--- templates/demo/config/namesystem.js
+++ templates/demo/config/namesystem.js
@@ -1,6 +1,12 @@
 module.exports = {
   default: {
     available_providers: ["ens"],
-    provider: "ens"
+    provider: "ens",
+    register: {
+      rootDomain: "embark.eth",
+      domains: {
+        'status': '0x1a2f3b98e434c02363f3dac3174af93c1d690914'
+      }
+    }
   }
 };
