920877808917297c5988688299a1d11bc64172f3
Jonathan Rainville <rainville.jonathan@gmail.com>
2018-08-06 13:57:19 -0400

    add back communicationContainer in layout

--- embark-ui/src/components/ExplorerLayout.js
+++ embark-ui/src/components/ExplorerLayout.js
@@ -10,8 +10,11 @@ import AccountsContainer from '../containers/AccountsContainer';
 import AccountContainer from '../containers/AccountContainer';
 import BlocksContainer from '../containers/BlocksContainer';
 import BlockContainer from '../containers/BlockContainer';
+import CommunicationContainer from '../containers/CommunicationContainer';
 import TransactionsContainer from '../containers/TransactionsContainer';
 import TransactionContainer from '../containers/TransactionContainer';
+
+const className = "d-flex align-items-center";
 
 const ExplorerLayout = () => (
   <Grid.Row>
@@ -20,7 +23,7 @@ const ExplorerLayout = () => (
       <div>
         <List.Group transparent={true}>
           <List.GroupItem
-            className="d-flex align-items-center"
+            className={className}
             to="/embark/explorer/accounts"
             icon="users"
             RootComponent={withRouter(NavLink)}
@@ -28,7 +31,7 @@ const ExplorerLayout = () => (
             Accounts
           </List.GroupItem>
           <List.GroupItem
-            className="d-flex align-items-center"
+            className={className}
             to="/embark/explorer/blocks"
             icon="book-open"
             RootComponent={withRouter(NavLink)}
@@ -36,12 +39,20 @@ const ExplorerLayout = () => (
             Blocks
           </List.GroupItem>
           <List.GroupItem
-            className="d-flex align-items-center"
+            className={className}
             to="/embark/explorer/transactions"
             icon="activity"
             RootComponent={withRouter(NavLink)}
           >
             Transactions
+          </List.GroupItem>
+          <List.GroupItem
+            className={className}
+            to="/embark/explorer/communication"
+            icon="phone-call"
+            RootComponent={withRouter(NavLink)}
+          >
+            Communication
           </List.GroupItem>
         </List.Group>
       </div>
@@ -52,6 +63,7 @@ const ExplorerLayout = () => (
         <Route exact path="/embark/explorer/accounts/:address" component={AccountContainer} />
         <Route exact path="/embark/explorer/blocks" component={BlocksContainer} />
         <Route exact path="/embark/explorer/blocks/:blockNumber" component={BlockContainer} />
+        <Route exact path="/embark/explorer/communication" component={CommunicationContainer} />
         <Route exact path="/embark/explorer/transactions" component={TransactionsContainer} />
         <Route exact path="/embark/explorer/transactions/:hash" component={TransactionContainer} />
       </Switch>
