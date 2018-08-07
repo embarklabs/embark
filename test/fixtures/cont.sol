8e396a17d8cc6449c97a98be1bf914e0e5622f58
Andre Medeiros <me@andremedeiros.info>
2018-08-07 15:26:39 -0400

    Progress

---
+++ test/fixtures/cont.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.4.24;
+
+contract x {
+  int number;
+  string name;
+
+  constructor(string _name)
+  public
+  {
+    name = _name;
+  }
+
+  function g(int _number)
+  public
+  returns (int _multiplication)
+  {
+    number = _number;
+    return _number * 5;
+  }
+
+  function f(int _foo, int _bar)
+  public
+  pure
+  returns (int _addition)
+  {
+    return _foo + _bar;
+  }
+
+  function h(int _bar)
+  public
+  pure
+  returns (bool _great)
+  {
+    if(_bar > 25) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
