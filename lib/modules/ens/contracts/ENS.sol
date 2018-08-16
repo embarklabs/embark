75fae6a964c45f8f9e37ae647d308737c9d55fa7
Anthony Laibe <anthony@laibe.cc>
2018-08-16 16:34:47 +0100

    Remove ENS warning

--- lib/modules/ens/contracts/ENS.sol
+++ lib/modules/ens/contracts/ENS.sol
@@ -15,12 +15,12 @@ interface ENS {
     event NewTTL(bytes32 indexed node, uint64 ttl);
 
 
-    function setSubnodeOwner(bytes32 node, bytes32 label, address owner) public;
-    function setResolver(bytes32 node, address resolver) public;
-    function setOwner(bytes32 node, address owner) public;
-    function setTTL(bytes32 node, uint64 ttl) public;
-    function owner(bytes32 node) public view returns (address);
-    function resolver(bytes32 node) public view returns (address);
-    function ttl(bytes32 node) public view returns (uint64);
+    function setSubnodeOwner(bytes32 node, bytes32 label, address owner) external;
+    function setResolver(bytes32 node, address resolver) external;
+    function setOwner(bytes32 node, address owner) external;
+    function setTTL(bytes32 node, uint64 ttl) external;
+    function owner(bytes32 node) external view returns (address);
+    function resolver(bytes32 node) external view returns (address);
+    function ttl(bytes32 node) external view returns (uint64);
 
 }
