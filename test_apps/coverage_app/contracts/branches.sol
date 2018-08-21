2ebb6d6ba4fdb5e76072dfa6156f70a47a658e85
Andre Medeiros <me@andremedeiros.info>
2018-08-21 15:25:18 -0400

    Improvements

---
+++ test_apps/coverage_app/contracts/branches.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.4.23;
+
+contract Branches {
+  uint public lastComparison;
+  uint public storedData;
+
+  constructor(uint initialValue) public {
+    storedData = initialValue;
+  }
+
+  function bigger(uint x) public returns (uint biggest) {
+    lastComparison = x;
+
+    if(x > storedData) {
+      storedData = x;
+      return x;
+    } else {
+      return storedData;
+    }
+  }
+
+  function smaller(uint x) public returns (uint smallest) {
+    lastComparison = x;
+
+    if(x < storedData) {
+      return x;
+    } else {
+      return storedData;
+    }
+  }
+
+  function get() public view returns (uint retVal) {
+    return storedData;
+  }
+}
