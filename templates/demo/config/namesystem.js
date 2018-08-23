dbd045aa323718857ea025f781c2ca0ef5875a8f
Anthony Laibe <anthony@laibe.cc>
2018-08-23 10:23:52 +0100

    Change ENS root to eth in demo

--- templates/demo/config/namesystem.js
+++ templates/demo/config/namesystem.js
@@ -6,9 +6,9 @@ module.exports = {
 
   development: {
     register: {
-      rootDomain: "embark.eth",
+      rootDomain: "eth",
       subdomains: {
-        'status': '0x1a2f3b98e434c02363f3dac3174af93c1d690914'
+        'embark': '0x1a2f3b98e434c02363f3dac3174af93c1d690914'
       }
     }
   }
