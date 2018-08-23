7472e2996aa7be6c12b017280e0d3085feb6f98d
Iuri Matias <iuri.matias@gmail.com>
2018-08-23 15:53:33 -0400

    fix configs

--- templates/boilerplate/config/namesystem.js
+++ templates/boilerplate/config/namesystem.js
@@ -14,7 +14,7 @@ module.exports = {
         'status': '0x1a2f3b98e434c02363f3dac3174af93c1d690914'
       }
     }
-  }
+  },
 
   // merges with the settings in default
   // used with "embark run privatenet"
