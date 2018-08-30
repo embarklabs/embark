a38625107f811985d833af4181874a6d01de271b
Anthony Laibe <anthony@laibe.cc>
2018-08-30 15:45:01 +0100

    Console can be a client or server ipc

--- cmd/cmd_controller.js
+++ cmd/cmd_controller.js
@@ -80,7 +80,8 @@ class EmbarkController {
       context: self.context,
       useDashboard: options.useDashboard,
       webServerConfig: webServerConfig,
-      webpackConfigName: options.webpackConfigName
+      webpackConfigName: options.webpackConfigName,
+      ipcRole: 'server'
     });
     engine.init();
 
@@ -258,7 +259,6 @@ class EmbarkController {
       logFile: options.logFile,
       logLevel: options.logLevel,
       context: this.context,
-      ipcRole: 'client',
       webpackConfigName: options.webpackConfigName
     });
     engine.init();
