426f71fd986bb2bf7c294d8160c0960618ee072e
Jonathan Rainville <rainville.jonathan@gmail.com>
2018-09-05 13:08:13 -0400

    convert ansi colors to spans

--- embark-ui/src/components/Process.js
+++ embark-ui/src/components/Process.js
@@ -1,16 +1,18 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {Page} from "tabler-react";
+import Convert from 'ansi-to-html';
+const convert = new Convert();
 
 class Process extends Component {
   render() {
     const {processLogs, process}= this.props;
     return (
-      <Page.Content className="text-capitalize" title={process.name}>
+      <Page.Content title={process.name}>
         <p className="text-capitalize">State: {process.state}</p>
         <div className="logs">
           {
-            processLogs.map((item, i) => <p key={i} className={item.logLevel}>{item.msg_clear || item.msg}</p>)
+            processLogs.map((item, i) => <p key={i} className={item.logLevel} dangerouslySetInnerHTML={{__html: convert.toHtml(item.msg)}}></p>)
           }
         </div>
       </Page.Content>
