53780aaa966f13a915e687affc3dcef89ee7ee93
Jordan Last <jordan.michael.last@gmail.com>
2018-09-07 15:58:12 -0600

    feat(@embark/cli): repl support inside dashboard
    Closes #768

--- src/cmd/dashboard/repl.js
+++ src/cmd/dashboard/repl.js
@@ -6,17 +6,25 @@ class REPL {
   constructor(options) {
     this.events = options.events;
     this.env = options.env;
+    this.inputStream = options.inputStream || process.stdin;
+    this.outputStream = options.outputStream || process.stdout;
+    this.logText = options.logText;
   }
 
   enhancedEval(cmd, context, filename, callback) {
     this.events.request('console:executeCmd', cmd.trim(), function (err, message) {
-      callback(err, message || ''); // This way, we don't print undefined
+      callback(err, message === undefined ? '' : message); // This way, we don't print undefined
     });
   }
 
   enhancedWriter(output) {
     if ((typeof output) === "string") {
+      if (this.logText) this.logText.log(output);
       return output;
+    } else {
+      const inspectedOutput = util.inspect(output, {colors: true});
+      if (this.logText) this.logText.log(inspectedOutput);
+      return inspectedOutput;
     }
     return util.inspect(output, {colors: true});
   }
@@ -26,7 +34,10 @@ class REPL {
       prompt: "Embark (" + this.env + ") > ",
       useGlobal: true,
       eval: this.enhancedEval.bind(this),
-      writer: this.enhancedWriter.bind(this)
+      writer: this.enhancedWriter.bind(this),
+      input: this.inputStream,
+      output: this.outputStream,
+      terminal: true
     });
 
     this.events.request('console:history', (err, history) => {
