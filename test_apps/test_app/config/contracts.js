257b27027d904a7c435b86df6f868faf24590c89
Iuri Matias <iuri.matias@gmail.com>
2018-09-15 12:20:20 -0400

    fix dashboard state so it doesn't show pending for contracts that were not deployed due to deployIf directive

--- test_apps/test_app/config/contracts.js
+++ test_apps/test_app/config/contracts.js
@@ -53,7 +53,7 @@ module.exports = {
         }
       },
       SomeContract: {
-        deployIf: 'MyToken.methods.isAvailable().call()',
+        deployIf: 'await MyToken.methods.isAvailable().call()',
         args: [
           ["$MyToken2", "$SimpleStorage"],
           100
