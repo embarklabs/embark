8fd1f8194934e4bd2945d121e18099b66324d73e
emizzle <ericmastro@status.im>
2018-09-27 15:45:59 +1000

    Storage configs: Move “provider” keys to the correct level

--- test_apps/coverage_app/config/storage.js
+++ test_apps/coverage_app/config/storage.js
@@ -2,9 +2,9 @@ module.exports = {
   default: {
     enabled: true,
     ipfs_bin: "ipfs",
-    provider: "ipfs",
     available_providers: ["ipfs"],
     upload: {
+      provider: "ipfs",
       host: "localhost",
       port: 5001
     },
@@ -25,8 +25,8 @@ module.exports = {
   },
   development: {
     enabled: true,
-    provider: "ipfs",
     upload: {
+      provider: "ipfs",
       host: "localhost",
       port: 5001,
       getUrl: "http://localhost:8080/ipfs/"
