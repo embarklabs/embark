7e12f80edaa6829c8ba9741f6035536ca255fec8
Anthony Laibe <anthony@laibe.cc>
2018-09-28 15:06:58 +0100

    Address PR feedback

--- cmd/cmd_controller.js
+++ cmd/cmd_controller.js
@@ -286,9 +286,7 @@ class EmbarkController {
         engine.startService("namingSystem");
         engine.startService("console");
         engine.startService("pluginCommand");
-        engine.events.on('check:backOnline:Ethereum', function () {
-          callback();
-        });
+        engine.events.on('check:backOnline:Ethereum', () => callback());
       },
       function ipcConnect(callback) {
         // Do specific work in case we are connected to a socket:
