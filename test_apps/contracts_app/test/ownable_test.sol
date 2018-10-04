ee77ac7f7f3864765de86e0d41df482e94b89077
Subramanian Venkatesan <subramanian.bsv@gmail.com>
2018-10-04 17:10:41 +0530

    Adds solc option to tests. Runs smart contract tests using remix-tests Ref: #817

---
+++ test_apps/contracts_app/test/ownable_test.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.7;
+import "./../ownable.sol";
+
+contract OwnableTests {
+  Ownable own;
+
+  function beforeAll() {
+    own = new Ownable();
+  }
+
+  function beforeEach() {
+  }
+
+  function shouldnotbezeroAddress() public {
+    Assert.equal(true, true, "owner is uninitialized");
+  }
+}
