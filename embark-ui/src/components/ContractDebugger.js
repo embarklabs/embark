29ebd9c8e06ab20995da5652d1cdf50136e35f68
Iuri Matias <iuri.matias@gmail.com>
2018-10-23 21:52:47 -0400

    update contract debugger to not user tabler react anymore

--- embark-ui/src/components/ContractDebugger.js
+++ embark-ui/src/components/ContractDebugger.js
@@ -1,10 +1,6 @@
 import PropTypes from "prop-types";
 import React, {Component} from 'react';
 import {
-  Page,
-  Grid, Table
-} from "tabler-react";
-import {
   Row,
   Col,
   FormGroup,
@@ -61,34 +57,28 @@ class ContractDebugger extends Component {
 
   render() {
     return (
-      <Page.Content title={this.props.contract.className + ' Debugger'}>
-        <Grid.Row>
-          <Grid.Col>
-            <Input name="txHash" id="txHash" onChange={(e) => this.handleChange(e)}/>
-            <Button color="primary" onClick={(e) => this.debug(e)}>Debug Tx</Button>
-          </Grid.Col>
-        </Grid.Row>
+      <Row>
+        <Col>
+          <Input name="txHash" id="txHash" onChange={(e) => this.handleChange(e)}/>
+          <Button color="primary" onClick={(e) => this.debug(e)}>Debug Tx</Button>
+        </Col>
 
-        <Grid.Row>
-          <Grid.Col>
-            <Button color="light" className="btn-square debugButton jumpBack" alt="jump to previous breakpoint" onClick={(e) => this.debugJumpBack(e)}></Button>
-            <Button color="light" className="btn-square debugButton jumpForward" alt="jump to revious breakpoint" onClick={(e) => this.debugJumpForward(e)}></Button>
-            <Button color="light" className="btn-square debugButton stepOverBack" alt="step back" onClick={(e) => this.debugStepOverBackward(e)}></Button>
-            <Button color="light" className="btn-square debugButton stepOverForward" alt="step over" onClick={(e) => this.debugStepOverForward(e)}></Button>
-            <Button color="light" className="btn-square debugButton stepIntoForward" alt="step into" onClick={(e) => this.debugStepIntoForward(e)}></Button>
-            <Button color="light" className="btn-square debugButton stepIntoBack" alt="step out" onClick={(e) => this.debugStepIntoBackward(e)}></Button>
-          </Grid.Col>
-        </Grid.Row>
+        <Col>
+          <Button color="light" className="btn-square debugButton jumpBack" alt="jump to previous breakpoint" onClick={(e) => this.debugJumpBack(e)}></Button>
+          <Button color="light" className="btn-square debugButton jumpForward" alt="jump to revious breakpoint" onClick={(e) => this.debugJumpForward(e)}></Button>
+          <Button color="light" className="btn-square debugButton stepOverBack" alt="step back" onClick={(e) => this.debugStepOverBackward(e)}></Button>
+          <Button color="light" className="btn-square debugButton stepOverForward" alt="step over" onClick={(e) => this.debugStepOverForward(e)}></Button>
+          <Button color="light" className="btn-square debugButton stepIntoForward" alt="step into" onClick={(e) => this.debugStepIntoForward(e)}></Button>
+          <Button color="light" className="btn-square debugButton stepIntoBack" alt="step out" onClick={(e) => this.debugStepIntoBackward(e)}></Button>
+        </Col>
 
-        <Grid.Row>
-          <Grid.Col>
-            <br /><strong>Scopes</strong>
-            <div>
-              <ReactJson src={{locals: this.props.debuggerInfo.locals, contract: this.props.debuggerInfo.globals}} theme="monokai" sortKeys={true} name={false} collapse={1} />
-            </div>
-          </Grid.Col>
-        </Grid.Row>
-      </Page.Content>
+        <Col>
+          <br /><strong>Scopes</strong>
+          <div>
+            <ReactJson src={{locals: this.props.debuggerInfo.locals, contract: this.props.debuggerInfo.globals}} theme="monokai" sortKeys={true} name={false} collapse={1} />
+          </div>
+        </Col>
+      </Row>
     );
   }
 }
