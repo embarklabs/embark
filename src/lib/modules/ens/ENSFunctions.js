embarklabs/embark

commit 2dfaaa8201c664efb99e54bc7a0b3b3ea07028bd

refactor(@embark/ens): use ZERO_ADDRESS constant

src/lib/modules/ens/ENSFunctions.js
@@ -1,10 +1,10 @@
+import { ZERO_ADDRESS } from '../../utils/addressUtils';
 /*global web3*/
 const namehash = require('eth-ens-namehash');
 // Price of ENS registration contract functions
 const ENS_GAS_PRICE = 700000;
 
 const reverseAddressSuffix = '.addr.reverse';
-const voidAddr = '0x0000000000000000000000000000000000000000';
 const NoDecodeAddrErr = 'Error: Couldn\'t decode address from ABI: 0x';
 const NoDecodeStringErr = 'ERROR: The returned value is not a convertible string: 0x0';
 
@@ -66,7 +66,7 @@ function lookupAddress(address, ens, utils, createResolverContract, callback) {
     if (err) {
       return cb(err);
     }
-    if (resolverAddress === voidAddr) {
+    if (resolverAddress === ZERO_ADDRESS) {
       return cb('Address not associated to a resolver');
     }
     createResolverContract(resolverAddress, (_, resolverContract) => {
@@ -90,7 +90,7 @@ function resolveName(name, ens, createResolverContract, callback) {
     if (err) {
       return cb(err);
     }
-    if (resolverAddress === voidAddr) {
+    if (resolverAddress === ZERO_ADDRESS) {
       return cb('Name not yet registered');
     }
     createResolverContract(resolverAddress, (_, resolverContract) => {

src/lib/modules/ens/index.js
@@ -5,6 +5,7 @@ const async = require('async');
 const embarkJsUtils = require('embarkjs').Utils;
 const reverseAddrSuffix = '.addr.reverse';
 const ENSFunctions = require('./ENSFunctions');
+import { ZERO_ADDRESS } from '../../utils/addressUtils';
 
 const MAINNET_ID = '1';
 const ROPSTEN_ID = '3';
@@ -177,7 +178,7 @@ class ENS {
           if (err) {
             return cb(err);
           }
-          if (resolverAddress === '0x0000000000000000000000000000000000000000') {
+          if (resolverAddress === ZERO_ADDRESS) {
             return cb('Name not yet registered');
           }
           next(null, resolverAddress);
@@ -455,7 +456,7 @@ class ENS {
           if (err) {
             return next(err);
           }
-          if(resolverAddress === '0x0000000000000000000000000000000000000000') {
+          if(resolverAddress === ZERO_ADDRESS) {
             return next('Name not yet registered');
           }
           next(null, resolverAddress);

commit 2f086801f2dfb7ac604e96c6dfd10690fea091da

fix(@embark/ens): use local ZERO_ADDRESS in ENSFunctions
We've introduced a regression in https://github.com/embark-framework/embark/commit/2dfaaa8201c664efb99e54bc7a0b3b3ea07028bd where zero addresses
have been replaced with a constant, including the ones within ENSFunctions.

The problem is that this code is as well added to the EmbarkJS client, where importing
`../../utils/addressUtils` won't work as it isn't available.

This commit reverts back to have ENSFunctions having its own ZERO_ADDRESS constant.

src/lib/modules/ens/ENSFunctions.js
@@ -1,9 +1,9 @@
-import { ZERO_ADDRESS } from '../../utils/addressUtils';
 /*global web3*/
 const namehash = require('eth-ens-namehash');
 // Price of ENS registration contract functions
 const ENS_GAS_PRICE = 700000;
 
+const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 const reverseAddressSuffix = '.addr.reverse';
 const NoDecodeAddrErr = 'Error: Couldn\'t decode address from ABI: 0x';
 const NoDecodeStringErr = 'ERROR: The returned value is not a convertible string: 0x0';