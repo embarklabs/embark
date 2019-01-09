eeaa781fbb341b028db2b564e1f5dbe2976430d5
Jonathan Rainville <rainville.jonathan@gmail.com>
2019-01-09 09:21:16 -0500

    change to use an object instead and change templates

--- test_apps/embark_demo/config/blockchain.js
+++ test_apps/embark_demo/config/blockchain.js
@@ -4,11 +4,15 @@ module.exports = {
     enabled: true,
     rpcHost: "localhost", // HTTP-RPC server listening interface (default: "localhost")
     rpcPort: 8545, // HTTP-RPC server listening port (default: 8545)
-    rpcCorsDomain: "auto",  // Comma separated list of domains from which to accept cross origin requests (browser enforced)
-                            // When set to "auto", Embark will automatically set the cors to the address of the webserver
+    rpcCorsDomain: { // Domains from which to accept cross origin requests (browser enforced). This can also be a comma separated list
+      auto: true, // When "auto" is true, Embark will automatically set the cors to the address of the webserver
+      additionalCors: [] // Additional CORS domains to add to the list. If "auto" is false, only those will be added
+    },
     wsRPC: true, // Enable the WS-RPC server
-    wsOrigins: "auto",  // Origins from which to accept websockets requests
-                        // When set to "auto", Embark will automatically set the cors to the address of the webserver
+    wsOrigins: { // Same thing as "rpcCorsDomain", but for WS origins
+      auto: true,
+      additionalCors: []
+    },
     wsHost: "localhost", // WS-RPC server listening interface (default: "localhost")
     wsPort: 8546 // WS-RPC server listening port (default: 8546)
   },
