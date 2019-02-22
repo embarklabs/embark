07fe1c6eb45378128f936db5a0066c77581bd9ae
Jonathan Rainville <rainville.jonathan@gmail.com>
2019-02-22 10:55:21 -0500

    chore: rename web3connector to embarkjs-web3-connector

--- packages/embarkjs-web3-connector/embarkJSWeb3Connector.js
+++ packages/embarkjs-web3-connector/embarkJSWeb3Connector.js
@@ -1,7 +1,7 @@
 /*global Web3*/
-const web3Connector = {};
+const embarkJSWeb3Connector = {};
 
-web3Connector.init = function(config) {
+embarkJSWeb3Connector.init = function(config) {
   global.web3 = config.web3 || global.web3;
   // Check if the global web3 object uses the old web3 (0.x)
   if (global.web3 && typeof global.web3.version !== 'string') {
@@ -13,46 +13,46 @@ web3Connector.init = function(config) {
   global.web3 = this.web3;
 };
 
-web3Connector.getInstance = function () {
+embarkJSWeb3Connector.getInstance = function () {
   return this.web3;
 };
 
-web3Connector.getAccounts = function () {
+embarkJSWeb3Connector.getAccounts = function () {
   return this.web3.eth.getAccounts(...arguments);
 };
 
-web3Connector.getNewProvider = function (providerName, ...args) {
+embarkJSWeb3Connector.getNewProvider = function (providerName, ...args) {
   return new Web3.providers[providerName](...args);
 };
 
-web3Connector.setProvider = function (provider) {
+embarkJSWeb3Connector.setProvider = function (provider) {
   return this.web3.setProvider(provider);
 };
 
-web3Connector.getCurrentProvider = function () {
+embarkJSWeb3Connector.getCurrentProvider = function () {
   return this.web3.currentProvider;
 };
 
-web3Connector.getDefaultAccount = function () {
+embarkJSWeb3Connector.getDefaultAccount = function () {
   return this.web3.eth.defaultAccount;
 };
 
-web3Connector.setDefaultAccount = function (account) {
+embarkJSWeb3Connector.setDefaultAccount = function (account) {
   this.web3.eth.defaultAccount = account;
 };
 
-web3Connector.newContract = function (options) {
+embarkJSWeb3Connector.newContract = function (options) {
   return new this.web3.eth.Contract(options.abi, options.address);
 };
 
-web3Connector.send = function () {
+embarkJSWeb3Connector.send = function () {
   return this.web3.eth.sendTransaction(...arguments);
 };
 
-web3Connector.toWei = function () {
+embarkJSWeb3Connector.toWei = function () {
   return this.web3.toWei(...arguments);
 };
 
-web3Connector.getNetworkId = function () {
+embarkJSWeb3Connector.getNetworkId = function () {
   return this.web3.eth.net.getId();
 };
