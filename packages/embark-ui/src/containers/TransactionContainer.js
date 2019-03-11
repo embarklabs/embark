74847ee32398aed7bf4e1d917c63f603d1585777
Jonathan Rainville <rainville.jonathan@gmail.com>
2019-03-11 09:49:16 -0400

    feat(cockpit/transaction): display a link for contracts and accounts

--- packages/embark-ui/src/containers/TransactionContainer.js
+++ packages/embark-ui/src/containers/TransactionContainer.js
@@ -3,15 +3,16 @@ import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import {withRouter} from 'react-router-dom';
 
-import {transaction as transactionAction, contracts as contractsAction} from '../actions';
+import {transaction as transactionAction, contracts as contractsAction, accounts as accountsAction} from '../actions';
 import Transaction from '../components/Transaction';
 import DataWrapper from "../components/DataWrapper";
 import PageHead from "../components/PageHead";
-import {getTransaction, getContracts} from "../reducers/selectors";
+import {getTransaction, getContracts, getAccounts} from "../reducers/selectors";
 
 class TransactionContainer extends Component {
   componentDidMount() {
     this.props.fetchContracts();
+    this.props.fetchAccounts();
     this.props.fetchTransaction(this.props.match.params.hash);
   }
 
@@ -20,7 +21,7 @@ class TransactionContainer extends Component {
       <React.Fragment>
         <PageHead title={`Transaction ${this.props.match.params.hash}`} description={`View details of transaction ${this.props.match.params.hash}`} />
         <DataWrapper shouldRender={this.props.transaction !== undefined } {...this.props} render={({transaction}) => (
-          <Transaction contracts={this.props.contracts} transaction={transaction} />
+          <Transaction contracts={this.props.contracts} transaction={transaction} accounts={this.props.accounts} />
         )} />
       </React.Fragment>
     );
@@ -31,6 +32,7 @@ function mapStateToProps(state, props) {
   return {
     transaction: getTransaction(state, props.match.params.hash),
     contracts: getContracts(state),
+    accounts: getAccounts(state),
     error: state.errorMessage,
     loading: state.loading
   };
@@ -40,6 +42,7 @@ TransactionContainer.propTypes = {
   match: PropTypes.object,
   transaction: PropTypes.object,
   contracts: PropTypes.arrayOf(PropTypes.object),
+  accounts: PropTypes.arrayOf(PropTypes.object),
   fetchContracts: PropTypes.func,
   fetchTransaction: PropTypes.func,
   error: PropTypes.string
@@ -49,6 +52,7 @@ export default withRouter(connect(
   mapStateToProps,
   {
     fetchContracts: contractsAction.request,
-    fetchTransaction: transactionAction.request
+    fetchTransaction: transactionAction.request,
+    fetchAccounts: accountsAction.request
   }
 )(TransactionContainer));
