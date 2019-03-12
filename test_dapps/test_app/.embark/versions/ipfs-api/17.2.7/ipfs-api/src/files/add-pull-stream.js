5c2e30c26c5965e56561d4450e2a4590c086b22f
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2019-03-12 10:17:33 -0500

    fix: remove unneeded test_dapps/ directory in the monorepo root

--- test_dapps/test_app/.embark/versions/ipfs-api/17.2.7/ipfs-api/src/files/add-pull-stream.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-const SendFilesStream = require('../utils/send-files-stream')
-const toPull = require('stream-to-pull-stream')
-
-module.exports = (send) => (options) => toPull(SendFilesStream(send, 'add')(options))
