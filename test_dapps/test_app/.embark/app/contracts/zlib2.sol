ddbfc3c518e04f4f54ab69e1b2f6e915d7221aac
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2019-03-12 10:17:33 -0500

    fix: remove unneeded test_dapps/ directory in the monorepo root

--- test_dapps/test_app/.embark/app/contracts/zlib2.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.17;
-
-library ZAMyLib2 {
-
-  function add(uint _a, uint _b) public pure returns (uint _c) {
-    return _a + _b;
-  }
-
-}
--- test_dapps/test_app/.embark/versions/ipfs-api/17.2.7/ipfs-api/src/files/add-readable-stream.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-const SendFilesStream = require('../utils/send-files-stream')
-
-module.exports = (send) => SendFilesStream(send, 'add')
