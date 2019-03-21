9829e925b4157b1b58f4fcf55652139ee02f72d5
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2019-03-21 10:36:24 -0500

    refactor(@embark/api): catch-all route, fallback pages for Cockpit
    Implement a `/*` server-side catch-all route for Cockpit that loads Cockpit's
    `index.html`.
    
    This change is necessary with intent to disable offline-first behavior in
    production builds of Cockpit. Cockpit's service worker effectively translates
    server-side route unavailability into client-side behaviors of
    connected-react-router. When the service worker is unregistered the same will
    be accomplished via the server-side catch-all route.
    
    Implement fallback pages for when embark is in the monorepo but Cockpit's
    Create React App development server isn't yet started or isn't yet responsive.
    
    Implement a fallback page for when the static build of Cockpit is missing. When
    embark is in the monorepo, give instructions for building Cockpit. Otherwise,
    report that the distribution is broken.
    
    Deprecate the environment variable `EMBARK_DEVELOPMENT` in favor of
    `EMBARK_UI_STATIC`. Unless the latter is truthy at runtime, when embark is in
    the monorepo the CRA dev server of Cockpit will be accessible at
    `localhost:55555` via proxied requests to `localhost:3000`. The deprecation is
    not a breaking change as `EMBARK_DEVELOPMENT` / `EMBARK_UI_STATIC` are not
    relevant to normal users, but only to developers working on embark itself.
    
    Bump `express-http-proxy` to the latest version.

--- packages/embark/src/lib/modules/api/server.ts
+++ packages/embark/src/lib/modules/api/server.ts
@@ -1,7 +1,7 @@
 import bodyParser from "body-parser";
 import cors from "cors";
-import { Embark, Plugins } from "embark";
-import express, { NextFunction, Request, Response } from "express";
+import {Embark, Plugins} from "embark";
+import express, {NextFunction, Request, Response} from "express";
 import proxy from "express-http-proxy";
 import expressWs from "express-ws";
 import findUp from "find-up";
@@ -11,7 +11,7 @@ import {__} from "i18n";
 import * as path from "path";
 import * as ws from "ws";
 // @ts-ignore
-import { embarkPath } from "../../core/fs";
+import {embarkPath, existsSync} from "../../core/fs";
 
 type Method = "get" | "post" | "ws" | "delete";
 
@@ -22,8 +22,14 @@ interface CallDescription {
 }
 
 export default class Server {
-  private isLogging: boolean = false;
+  private _isInsideMonorepo: boolean | null = null;
+  private _monorepoRootDir: string = "";
+  private embarkUiBuildDir: string = (
+    findUp.sync("node_modules/embark-ui/build", {cwd: embarkPath()}) ||
+    embarkPath("node_modules/embark-ui/build")
+  );
   private expressInstance: expressWs.Instance;
+  private isLogging: boolean = false;
   private server?: http.Server;
 
   constructor(private embark: Embark, private port: number, private hostname: string, private plugins: Plugins) {
@@ -36,6 +42,22 @@ export default class Server {
 
   public disableLogging() {
     this.isLogging = false;
+  }
+
+  private get isInsideMonorepo() {
+    if (this._isInsideMonorepo === null) {
+      this._isInsideMonorepo = existsSync(embarkPath("../../packages/embark")) &&
+        existsSync(embarkPath("../../lerna.json")) &&
+        path.resolve(embarkPath("../../packages/embark")) === embarkPath();
+    }
+    return this._isInsideMonorepo;
+  }
+
+  private get monorepoRootDir() {
+    if (!this._monorepoRootDir && this.isInsideMonorepo) {
+      this._monorepoRootDir = path.resolve(embarkPath("../.."));
+    }
+    return this._monorepoRootDir;
   }
 
   public start() {
@@ -65,9 +87,78 @@ export default class Server {
     });
   }
 
+  private makePage(reloadSeconds: number, body: string) {
+    return (`
+      <!doctype html>
+      <html lang="en">
+        <head>
+          <meta charset="utf-8" />
+          ${this.isInsideMonorepo ? `
+            <meta http-equiv="refresh" content="${reloadSeconds}">
+          ` : ""}
+          <title>Embark API Server</title>
+          <style type="text/css">
+            code {
+              background-color: rgba(220,220,220,.5);
+            }
+            ${this.isInsideMonorepo ? `
+              #timer {
+                background-color: black;
+                color: yellow;
+                padding: 0.25em;
+              }
+            ` : ""}
+          </style>
+        </head>
+        <body>
+          ${body}
+          ${this.isInsideMonorepo ? `
+            <p>this page will automatically reload
+              in <span id="timer">${reloadSeconds}</span> seconds</p>
+            <script>
+              let timeLeft = ${reloadSeconds};
+              const span = document.querySelector("#timer");
+              setInterval(() => {
+                if (timeLeft >= 1) { timeLeft -= 1; }
+                span.innerText = \`\${timeLeft}\`;
+              }, 1000);
+            </script>
+          ` : ""}
+        </body>
+      </html>
+    `.trim().split("\n").map((str) => str.trim()).filter((str) => str).join("\n"));
+  }
+
+  private makePage404(reloadSeconds: number, envReport: string, inside: string, notice: string) {
+    return this.makePage(reloadSeconds, `
+      ${envReport}
+      <p>missing build for package <code>embark-ui</code> ${inside}</p>
+      ${notice}
+    `);
+  }
+
+  private makePageEConnError(reloadSeconds: number, waitingFor: string) {
+    return this.makePage(reloadSeconds, `
+      <p><code>lib/modules/api/server</code> inside the monorepo at
+        <code>${path.join(this.monorepoRootDir, "packages/embark")}</code> is
+        waiting for the Create React App development server of package
+        <code>embark-ui</code> to ${waitingFor} at
+        <code>localhost:55555</code></p>
+      ${waitingFor === "become available" ? `
+        <p>please run either:</p>
+        <p><code>cd ${this.monorepoRootDir} && yarn start</code><br />
+          or<br />
+          <code>cd ${path.join(this.monorepoRootDir, "packages/embark-ui")}
+            && yarn start</code></p>
+        <p>to instead use a static build from the monorepo, restart embark with:
+          <code>EMBARK_UI_STATIC=t embark run</code></p>
+      ` : ""}
+    `);
+  }
+
   private initApp() {
     const instance = expressWs(express());
-    instance.app.use((req: Request, res: Response, next: NextFunction) => {
+    instance.app.use((req: Request, _res, next: NextFunction) => {
       if (!this.isLogging) {
         return next();
       }
@@ -91,7 +182,7 @@ export default class Server {
     });
 
     if (this.plugins) {
-      instance.app.get("/embark-api/plugins", (req: Request, res: Response) => {
+      instance.app.get("/embark-api/plugins", (_req, res: Response) => {
         res.send(JSON.stringify(this.plugins.plugins.map((plugin) => ({name: plugin.name}))));
       });
 
@@ -101,17 +192,72 @@ export default class Server {
 
     this.embark.events.on("plugins:register:api", (callDescription: CallDescription) => this.registerCallDescription(instance, callDescription));
 
-    let ui: express.RequestHandler;
-    if (process.env.EMBARK_DEVELOPMENT) {
-      ui = proxy("http://localhost:3000");
+    if (!this.isInsideMonorepo || process.env.EMBARK_UI_STATIC) {
+      if (existsSync(path.join(this.embarkUiBuildDir, "index.html"))) {
+        instance.app.use("/", express.static(this.embarkUiBuildDir));
+        instance.app.get("/*", (_req, res) => {
+          res.sendFile(path.join(this.embarkUiBuildDir, "index.html"));
+        });
+      } else {
+        let envReport = "";
+        let inside = `
+          in <code>${path.dirname(this.embarkUiBuildDir)}</code>
+        `;
+        let notice = `
+          <p>this distribution of <code>embark-ui</code> appears to be broken</p>
+        `;
+        if (this.isInsideMonorepo) {
+          envReport = `
+            <p><code>process.env.EMBARK_UI_STATIC ===
+              ${JSON.stringify(process.env.EMBARK_UI_STATIC)}</code></p>
+          `;
+          inside = `
+            inside the monorepo at <code>
+              ${path.join(this.monorepoRootDir, "packages/embark-ui")}</code>
+          `;
+          notice = `
+            <p>to build <code>embark-ui</code> please run either:</p>
+            <p><code>cd ${this.monorepoRootDir} && yarn build</code><br />
+              or<br />
+              <code>cd ${path.join(this.monorepoRootDir, "packages/embark-ui")}
+                && yarn build</code></p>
+            <p>restart <code>embark run</code> after building
+              <code>embark-ui</code></p>
+            <p>to instead use a live development build from the monorepo, unset
+              the environment variable <code>EMBARK_UI_STATIC</code> and restart
+              embark</p>
+          `;
+        }
+        const page404 = this.makePage404(3, envReport, inside, notice);
+        const missingBuildHandler = (_req: Request, res: Response) => {
+          res.status(404).send(page404);
+        };
+        instance.app.get("/", missingBuildHandler);
+        instance.app.get("/*", missingBuildHandler);
+      }
     } else {
-      ui = express.static(
-        findUp.sync("node_modules/embark-ui/build", {cwd: embarkPath()}) ||
-          embarkPath("node_modules/embark-ui/build"),
-      );
+      const page503 = this.makePageEConnError(3, "become available");
+      const page504 = this.makePageEConnError(3, "become responsive");
+      instance.app.use("/", proxy("http://localhost:3000", {
+        // @ts-ignore
+        proxyErrorHandler: (err, res, next) => {
+          switch (err && err.code) {
+            case "ECONNREFUSED": {
+              return res.status(503).send(page503);
+            }
+            case "ECONNRESET": {
+              if (err.message === "socket hang up") {
+                return res.status(504).send(page504);
+              }
+            }
+            default: {
+              next(err);
+            }
+          }
+        },
+        timeout: 1000,
+      }));
     }
-
-    instance.app.use("/", ui);
 
     return instance;
   }
