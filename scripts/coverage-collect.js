ad01d1402cca41331e3caad40267cf6f4c7e4b90
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2019-04-28 16:33:41 -0500

    test: collect code coverage in the root of the monorepo
    Implement scripts to collect coverage reports (JSON format) from all packages
    in the monorepo that generate such reports. Reports are copied to
    `<root>/.nyc_output/coverage-[pkg-dir-name].json`.
    
    Implement scripts to generate a combined html report in `<root>/coverage`.
    
    Adjust root `reset` and `clean` scripts to delete `<root>/.nyc_output` and
    `<root>/coverage`.
    
    Implement a script in `<root>/package.json` to generate a `text-lcov` report
    and upload it to coveralls from CI builds. Remove coveralls from
    `packages/embark`.
    
    Supply `packages/embark` with an nyc configuration in its `package.json` and
    have its `"test":` script generate both `json` and `html` reports.
    
    Use nyc with `embarkjs`'s test suite: supply an nyc configuration in its
    `package.json` and have its `"test":` script generate both `json` and `html`
    reports.
    
    Adjust `embarkjs`'s tests for more accurate coverage reporting.

---
+++ scripts/coverage-collect.js
@@ -0,0 +1,16 @@
+/* global __dirname require */
+
+const fs = require('fs-extra');
+const {execSync} = require('child_process');
+const path = require('path');
+const rimraf = require('util').promisify(require('rimraf'));
+
+const collector = path.join(__dirname, './coverage-collector.js');
+const destination = path.join(__dirname, '../.nyc_output');
+const cmd = `npx lerna exec --parallel 'node "${collector}" "${destination}"'`;
+
+(async () => {
+  await fs.mkdirp(destination);
+  await rimraf(path.join(destination, '*'));
+  execSync(cmd, {stdio: 'inherit'});
+})();
---
+++ scripts/coverage-collector.js
@@ -0,0 +1,12 @@
+/* global process require */
+
+const fs = require('fs');
+const path = require('path');
+
+const source = path.join(process.cwd(), 'coverage/coverage-final.json');
+const pkgName = path.basename(process.cwd());
+const destination = path.join(process.argv[2], `coverage-${pkgName}.json`);
+
+if (fs.existsSync(source)) {
+  fs.copyFileSync(source, destination);
+}
