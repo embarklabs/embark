92e6f43e39cc5dd821da16d0c638ba0910fc01ad
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2019-04-30 16:47:56 -0500

    ci: revise azure pipelines config to report branch and service name to coveralls

---
+++ scripts/coveralls-branch-name.js
@@ -0,0 +1,17 @@
+/* global process require */
+
+const {execSync} = require('child_process');
+
+const nameRevCmd = 'git name-rev --name-only HEAD';
+let branchName = execSync(nameRevCmd).toString().trim();
+
+if (branchName.startsWith('remotes/pull')) {
+  execSync('git checkout "HEAD^2"', {stdio: 'ignore'});
+  const _branchName = branchName;
+  branchName = execSync(nameRevCmd).toString().trim().split('/').slice(2).join('/');
+  execSync(`git checkout ${_branchName}`, {stdio: 'ignore'});
+} else if (branchName.startsWith('remotes/origin')) {
+  branchName = branchName.split('/').slice(2).join('/');
+}
+
+process.stdout.write(branchName);
