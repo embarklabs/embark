014161acc2cd22dfa9da051b6ea50e620f1a39d5
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2019-05-01 09:54:42 -0500

    ci: on azure pipelines split coveralls and ci:full invocation scripts, run each script conditionally

---
+++ scripts/coveralls-pr-id.js
@@ -0,0 +1,12 @@
+/* global process require */
+
+const {execSync} = require('child_process');
+
+let branchName = execSync('git name-rev --name-only HEAD').toString().trim();
+let prId = '';
+
+if (branchName.startsWith('remotes/pull')) {
+  prId = branchName.split('/')[2];
+}
+
+process.stdout.write(prId);
