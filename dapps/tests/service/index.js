6d0b680dc5379c479c4203298e8a28e7908f8aef
Jonathan Rainville <rainville.jonathan@gmail.com>
2019-06-07 11:05:16 -0400

    feat: add test env to embark test and disable modules by default

--- dapps/tests/service/index.js
+++ dapps/tests/service/index.js
@@ -11,7 +11,7 @@ module.exports = function (embark) {
 
   embark.registerContractConfiguration({
     "default": {
-      "contracts": {
+      "deploy": {
         "PluginStorage": {
           "args": ["$SimpleStorage"]
         }
