5e560768ad295b98d73a4d2377a9dc6833182b8b
André Medeiros <me@andremedeiros.info>
2019-09-07 15:55:24 -0400

    chore: test runner tests (#1884)
    * chore: test runner tests
    
    * chore: mocha test runner tests
    
    * chore: add solidity test file to dapp

--- packages/plugins/mocha-tests/src/test/mocha_tests_test.js
+++ packages/plugins/mocha-tests/src/test/mocha_tests_test.js
@@ -1,8 +1,47 @@
 /* globals describe, it */
 const assert = require('assert').strict;
+const refute = require('refute')(assert);
+const sinon = require('sinon');
+
+const MochaTestRunner = require('../lib');
+
+const validFiles = [
+  'SOME_UPPERCASE_TEST.js',
+  'other_uppercase_test.JS',
+  'some_test_file.js',
+  'c:\test\file.js'
+];
+
+const invalidFiles = [
+  'jsschool.com',
+  'other_uppercase_test.js.map'
+];
 
 describe('Mocha Test Runner', () => {
-  it('should have tests', (_done) => {
-    assert(false, 'No tests yet on embark-mocha-tests');
+  let embark;
+
+  beforeEach(() => {
+    const events = { request: sinon.spy() };
+    embark = { events: events };
+  });
+
+  describe('methods', () => {
+    let instance;
+
+    beforeEach(() => { instance = new MochaTestRunner(embark, {}); });
+
+    describe('match', () => {
+      it('matches .js files', () => {
+        validFiles.forEach(f => {
+          assert(instance.match(f), `didn't match ${f} when it should`);
+        });
+      });
+
+      it('does not match non .js files', () => {
+        invalidFiles.forEach(f => {
+          refute(instance.match(f), `matched ${f} when it shouldn't`);
+        });
+      });
+    });
   });
 });
