ad91fc320c1855d63c245b3ba94e93493c613d26
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2019-09-15 12:50:07 -0500

    refactor(@cockpit/ui): use non-ejected create-react-app
    Change components/TexEditor re: how monaco-editor is loaded, since difficulties
    with monaco-editor were the original motivation for "ejecting" the CRA setup.
    
    Specify "nohoist" for `embark-ui/react-scripts` and all its dependencies to
    avoid potential headaches when upgrading the react-scripts, i.e. upgrading to
    newer versions of create-react-app.

---
+++ packages/cockpit/cockpit-ui/scripts/copy-monaco-to-public.js
@@ -0,0 +1,29 @@
+/* global __dirname process require */
+
+const findUp = require('find-up');
+const {copy, ensureDir} = require('fs-extra');
+const path = require('path');
+
+const {promisify} = require('util');
+const rimraf = promisify(require('rimraf'));
+
+async function main() {
+  const which = process.argv[2] || 'min';
+  const monacoDir = await findUp('node_modules/monaco-editor', {type: 'directory'});
+  const vsDir = path.join(__dirname, '..', 'public', 'vsdir');
+  const innerVsDir = path.join(vsDir, 'vsdir');
+
+  await rimraf(vsDir);
+  await ensureDir(innerVsDir);
+  await copy(path.join(monacoDir, which), innerVsDir);
+
+  const minMapsDir = path.join(vsDir, 'min-maps');
+  await rimraf(minMapsDir);
+
+  if (which === 'min') {
+    await ensureDir(minMapsDir);
+    await copy(path.join(monacoDir, 'min-maps'), minMapsDir);
+  }
+}
+
+main();
