c8038484010e8c261685fcf79494bdc23467e93e
Jonathan Rainville <rainville.jonathan@gmail.com>
2019-09-27 15:11:36 -0400

    add more tests

--- packages/plugins/ens/test/index.js
+++ packages/plugins/ens/test/index.js
@@ -1,4 +1,5 @@
-const ENS = require("../src/index");
+const ENS = require("../dist/index");
+const { fakeEmbark } = require('embark-testing');
 
 describe('embark-ens', () => {
   let ens, doneCb;
@@ -7,7 +8,18 @@ describe('embark-ens', () => {
 
 
   beforeEach(() => {
+    embark.events.setCommandHandler('namesystem:node:register', () => {});
     ens = new ENS(embark);
+    ens.config = {
+      embarkConfig: {
+        generationDir: 'test-dir'
+      },
+      namesystemConfig: {
+        register: {
+          rootDomain: 'root.eth'
+        }
+      }
+    };
   });
 
   afterEach(() => {
@@ -15,19 +27,42 @@ describe('embark-ens', () => {
   });
 
   it("should register the right artifact", (done) => {
-
-    const pipelineRegisterHandler = jest.fn();
-
-    embark.events.setCommandHandler('pipeline:register', pipelineRegisterHandler);
-
-    ens.addArtifactFile({}, () => {
-      expect(pipelineRegisterHandler).toHaveBeenNthCalledWith({
-        path: [embark.config.embarkConfig.generationDir, 'config'],
+    const pipelineRegisterHandler = jest.fn((args, cb) => {
+      expect(args).toEqual({
+        path: ['test-dir', 'config'],
         file: 'namesystem.json',
         format: 'json',
         content: Object.assign({}, embark.config.namesystemConfig, ens.getEnsConfig())
       });
+      cb();
       done();
+    });
+    ens.getEnsConfig = jest.fn();
+
+    embark.events.setCommandHandler('pipeline:register', pipelineRegisterHandler);
+
+    ens.addArtifactFile({}, () => {});
+  });
+
+  describe('safeRegisterSubDomain', () => {
+    it('should register if the name is not registered', (done) => {
+      ens.ensResolve = jest.fn((name, cb) => {cb(null, null)});
+      ens.registerSubDomain = jest.fn((defaultAccount, subDomainName, reverseNode, address, secureSend, callback) => callback());
+
+      ens.safeRegisterSubDomain('test.eth', '0x0123', '0x4321', () => {
+        expect(ens.registerSubDomain).toHaveBeenCalled();
+        done();
+      });
+    });
+
+    it('should not register if the name is already registered', (done) => {
+      ens.ensResolve = jest.fn((name, cb) => {cb(null, '0x0123')});
+      ens.registerSubDomain = jest.fn((defaultAccount, subDomainName, reverseNode, address, secureSend, callback) => callback());
+
+      ens.safeRegisterSubDomain('test.eth', '0x0123', '0x4321', () => {
+        expect(ens.registerSubDomain).not.toHaveBeenCalled();
+        done();
+      });
     });
   });
 });
