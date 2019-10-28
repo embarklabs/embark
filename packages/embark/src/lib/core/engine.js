embarklabs/embark

commit ed0d3afb4ff11520e4a73d1318d1269c2e994c06

refactor(@embark/blockchain): move module into own package (#1942)

packages/embark/src/lib/core/engine.js
@@ -167,7 +167,7 @@ class Engine {
 
   stackComponents(options) {
     this.registerModulePackage('embark-pipeline', { plugins: this.plugins });
-    this.registerModule('blockchain', { plugins: this.plugins });
+    this.registerModulePackage('embark-blockchain', { plugins: this.plugins });
     this.registerModulePackage('embark-proxy', {plugins: this.plugins});
     // TODO: coverage param should be part of the request compilation command, not an option here
     // some other params in the options might not longer be relevant, in fact we probably don't need options anymore

packages/stack/blockchain/.npmrc
@@ -1,0 +1,4 @@
+engine-strict = true
+package-lock = false
+save-exact = true
+scripts-prepend-node-path = true

packages/stack/blockchain/src/api.ts
@@ -1,6 +1,4 @@
 import { Embark, Events, Logger } from "embark";
-import { Request, Response } from "express";
-import Websocket from "ws";
 export default class BlockchainAPI {
   private embark: Embark;
   private logger: Logger;

commit b3b48787bcee847be97a8b7d6748e11d3f43ee59

fix(@embark/core): make blockchain command work again
Running embark's `blockchain` command resulted in a runtime error where the `blockchain`
module couldn't be found. This is a bug introduced in https://github.com/embark-framework/embark/commit/ed0d3afb4ff11520e4a73d1318d1269c2e994c06 where
we forgot to update `blockchainStackComponents` in Embark's engine accordingly.

Fixing this results in `embark blockchain` hanging. This is because there's a similar bug
in `blockchainStackCopmnonents` introduced in https://github.com/embark-framework/embark/commit/3b8f8f9ea74a5c90d3a2c4a635b7b82841ff073f.

This commit fixes both bugs by ensuring `embark-blockchain` and `embark-blockchain-client`
packages are loaded using the correct APIs.

packages/embark/src/lib/core/engine.js
@@ -160,8 +160,8 @@ class Engine {
   }
 
   blockchainStackComponents() {
-    this.registerModule('blockchain', { plugins: this.plugins });
-    this.registerModule('blockchain-client');
+    this.registerModulePackage('embark-blockchain', { plugins: this.plugins });
+    this.registerModulePackage('embark-blockchain-client');
     this.registerModulePackage('embark-process-logs-api-manager');
   }
 