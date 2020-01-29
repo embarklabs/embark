5b988ead7a9d3bad426b9026de3c744deae0ad61
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2020-01-28 19:00:23 -0600

    refactor(@embark/core): refactor logger to provide correct origins
    Also make a few more revisions:
    
    Revise the "write logs" testing strategy such that it's not necessary for the
    logger functions to take an optional callback.
    
    Drop unused `tmp` package from `packages/core/logger` since it's not used in
    the tests.
    
    Strip colors before writing to the log file, use a two-space delimiter between
    sections of each logged line in the log file, and collapse whitespace in the
    message section of each line. These changes make the log file more amenable to
    being processed with cli tools such as awk, cut, etc. It's also possible in a
    text editor to replace `'  '` with `\t` and then load the file in a spreadsheet,
    with each line-section in its own column.
    
    Rearrange the sections of each logged line so that it's easier to read, and
    only include the origin if the loglevel is debug or trace.

--- packages/core/logger/test/logger.spec.js
+++ packages/core/logger/test/logger.spec.js
@@ -2,35 +2,49 @@ import assert from 'assert';
 import sinon from 'sinon';
 import * as fs from 'fs';
 import { fakeEmbark } from 'embark-testing';
-import { Logger } from '../src/';
-import tmp from 'tmp';
 
 describe('core/logger', () => {
 
   const { embark, plugins } = fakeEmbark();
 
-  let logger, testLogFn, logFile;
+  let logger, testLogFn, resolve;
+  const logFile = 'embark.log';
 
   const fsMock = {
     logs: {},
     appendFile: (file, content, callback) => {
+      fsMock.logs[file].push(content);
+      if (resolve) resolve();
+      callback();
+    },
+    ensureFileSync: (file) => {
       if (!fsMock.logs[file]) {
         fsMock.logs[file] = [];
       }
-      fsMock.logs[file].push(content);
-      callback();
     }
   };
 
-  beforeEach(() => {
-    logFile = tmp.fileSync();
-    fsMock[logFile.name] = [];
+  let Logger;
+  beforeAll(() => {
+    process.env.FORCE_COLOR = '1';
+    // load the module only after the environment variable is set, otherwise
+    // the colors package will already have been loaded and in CI the result
+    // will be that colors are disabled and tests below will fail
+    ({Logger} = require('../src'));
+  });
 
+  afterAll(() => {
+    delete process.env.FORCE_COLOR;
+  });
+
+  beforeEach(() => {
+    fsMock.logs[logFile] = [];
+    resolve = null;
     testLogFn = sinon.fake();
     logger = new Logger({
       events: embark.events,
+      logFile,
       logFunction: testLogFn,
-      logFile: logFile.name,
       fs: fsMock
     });
   });
@@ -40,37 +54,40 @@ describe('core/logger', () => {
     sinon.restore();
   });
 
-  test('it should use custom log function for logging', () => {
+  test('it should use custom log function for logging', async () => {
+    const promise = new Promise(res => { resolve = res; });
     logger.info('Hello world');
     assert(testLogFn.calledOnce);
+    await promise;
   });
 
-  test('it should inject color encoding based on log method', () => {
+  test('it should inject color encoding based on log method', async () => {
     logger.info('Hello world');
     assert(testLogFn.calledWith('\u001b[32mHello world\u001b[39m'));
     logger.warn('Hello world');
     assert(testLogFn.calledWith('\u001b[33mHello world\u001b[39m'));
+    const promise = new Promise(res => { resolve = res; });
     logger.error('Hello world');
     assert(testLogFn.calledWith('\u001b[31mHello world\u001b[39m'));
+    await promise;
   });
 
-  test('it should write logs to log file', (done) => {
-    const stats = fs.statSync(logFile.name);
-    logger.info('Some test log', () => {
-      fsMock.logs[logFile.name].forEach(entry => {
-        if (entry.indexOf('[info]:  Some test log') > -1) {
-          assert.ok(true);
-        }
-      });
-      done();
-    });
+  test('it should write logs to log file', async () => {
+    const promise = new Promise(res => { resolve = res; });
+    logger.info('Some test log');
+    await promise;
+    assert(fsMock.logs[logFile].some(
+      entry => entry.includes('[info]  Some test log')
+    ));
   });
 
-  test('it should not log if log method level is higher than configured log level', () => {
+  test('it should not log if log method level is higher than configured log level', async () => {
     logger.trace('Hello world');
     // default log level is `info` which is lower than `trace`
     assert.ok(!testLogFn.calledOnce);
+    const promise = new Promise(res => { resolve = res; });
     logger.warn('Test');
     assert.ok(testLogFn.calledOnce);
+    await promise;
   });
 });
