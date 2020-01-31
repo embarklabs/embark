5fc96c089fb1684a4e0badbf4c68df2c76dfc1f6
Jonathan Rainville <rainville.jonathan@gmail.com>
2020-01-31 15:10:26 -0500

    chore(@embarkens): update ENS Registry mainnet address
    Change address to the updated one because of a flaw found in the old

--- packages/embarkjs/ens/src/index.js
+++ packages/embarkjs/ens/src/index.js
@@ -148,7 +148,7 @@ const voidAddress = '0x0000000000000000000000000000000000000000';
 
 __embarkENS.registryAddresses = {
   // Mainnet
-  "1": "0x314159265dd8dbb310642f98f50c066173c1259b",
+  "1": "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e",
   // Ropsten
   "3": "0x112234455c3a32fd11230c42e7bccd4a84e02010",
   // Rinkeby
